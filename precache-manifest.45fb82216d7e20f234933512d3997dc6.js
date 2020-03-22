caf2e97a10912e6c76ffd9deb043c51e88a80333
igor29 <igmatviytsev@gmail.com>
2020-03-22 10:19:19 +0200

    Updates

---
+++ precache-manifest.45fb82216d7e20f234933512d3997dc6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2231c6ac79c16c9da9f2005878a22a",
+    "url": "/pronews/index.html"
+  },
+  {
+    "revision": "3f5e706e751500358779",
+    "url": "/pronews/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8eda2ceaba8bf1c0b456",
+    "url": "/pronews/static/css/main.6f472582.chunk.css"
+  },
+  {
+    "revision": "3f5e706e751500358779",
+    "url": "/pronews/static/js/2.78d13981.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pronews/static/js/2.78d13981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eda2ceaba8bf1c0b456",
+    "url": "/pronews/static/js/main.034c1cbc.chunk.js"
+  },
+  {
+    "revision": "c9e3a53df7e5dd010a82",
+    "url": "/pronews/static/js/runtime-main.819b7093.js"
+  },
+  {
+    "revision": "433488815f6fc27dbb016b1a97bd1d9f",
+    "url": "/pronews/static/media/logo-black.43348881.png"
+  },
+  {
+    "revision": "f81063f49338560aaafe5802fa023fdc",
+    "url": "/pronews/static/media/logo.f81063f4.png"
+  },
+  {
+    "revision": "26d9eed8004890c4b9df5b5f7a33506d",
+    "url": "/pronews/static/media/profile.26d9eed8.png"
+  }
+]);
